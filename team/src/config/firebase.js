74b14915e138da5b82b52c726a16cf7d3d96075b
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-11-04 17:12:34 +0100

    Firebase

---
+++ team/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ88bSwM-gwlN-3EH0E5Ps4bu2GeF94ss",
+  authDomain: "assembler-memes.firebaseapp.com",
+  projectId: "assembler-memes",
+  storageBucket: "assembler-memes.appspot.com",
+  messagingSenderId: "803544584075",
+  appId: "1:803544584075:web:9276d4cf32514100b2698e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
